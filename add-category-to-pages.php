391cf5b0278486aaba4057c9607a87374ffbb7f3
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2024-08-15 01:43:31 +0300

    Update add-category-to-pages.php

--- add-category-to-pages.php
+++ add-category-to-pages.php
@@ -1,38 +1,33 @@
 <?php
 /**
- * Plugin Name: Add Categories to Pages
+ * Plugin Name: Page Categorizer
  * Plugin URI: https://thewphosting.com/add-categories-tags-pages-wordpress/
  * Description: Easily add Categories and Tags to Pages. Simply activate and visit the Page Edit screen.
- * Author: a.ankit
- * Version: 1.3
+ * Author: WP Corner
+ * Version: 1.0.0
  * Author URI: https://thewphosting.com
  * License:  GPL2
- * Text Domain: add-category-to-pages
+ * Text Domain: page-categorizer
  * Requires at least: 5.7
  * Requires PHP: 5.6
  */
-
 // If this file is called directly, abort.
 if (!defined('ABSPATH')) {
     exit;
 }
-
-function acp_register_taxonomies() {
+function pc_register_taxonomies() {
     register_taxonomy_for_object_type('post_tag', 'page');
     register_taxonomy_for_object_type('category', 'page');
 }
-add_action('init', 'acp_register_taxonomies');
-
-function acp_modify_archive_query($wp_query) {
+add_action('init', 'pc_register_taxonomies');
+function pc_modify_archive_query($wp_query) {
     if ($wp_query->is_main_query() && !is_admin() && ($wp_query->is_category() || $wp_query->is_tag())) {
         $my_post_array = array('post', 'page');
-
         if ($wp_query->get('category_name') || $wp_query->get('cat'))
             $wp_query->set('post_type', $my_post_array);
-
         if ($wp_query->get('tag'))
             $wp_query->set('post_type', $my_post_array);
     }
 }
-add_action('pre_get_posts', 'acp_modify_archive_query');
+add_action('pre_get_posts', 'pc_modify_archive_query');
 ?>
